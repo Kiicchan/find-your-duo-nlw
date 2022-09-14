52fc3e00278568f3258645be2cf21c62512ce485
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-14 02:47:22 -0300

    aula 02 - mobile

---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+    logo: {
+        width: 214,
+        height: 120,
+        marginTop: 50
+    },
+    list: {
+        flexGrow: 0
+    },
+    contentList: {
+        paddingLeft: 32,
+        paddingRight: 64,
+    }
+});
